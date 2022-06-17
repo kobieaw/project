2b4a0f9cedb6b9a594d754611228188f545303c8
kobie <kwilliams65@bitbucket.org>
2022-06-17 00:42:12 -0400

    v5 commit with front-end outline done

--- project-FrontEnd/src/app/app.module.ts
+++ project-FrontEnd/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DashboardComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ project-FrontEnd/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css'],
+})
+export class DashboardComponent implements OnInit {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() {}
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  ngOnInit(): void {}
+}
